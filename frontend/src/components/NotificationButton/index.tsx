5f42ebc67aae0ccf1d67ecfd809ca8aa19ae72b3
Stenio Ellison <stenio1998@gmail.com>
2022-09-11 15:15:27 -0300

    Atualizando arquivos

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,4 @@
 import Icon from "../../assets/img/notification-icon.svg"
-import Icon2 from "../../assets/img/icon.svg"
 import "./styles.css"
 import  axios  from 'axios';
 import { BASE_URL } from "../../utils/request";
@@ -19,7 +18,7 @@ function NotificationButton( {saleId} : Props) {
     } 
     return (
         <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
-            <img src={Icon2} alt="Notificar" />
+            <img src={Icon} alt="Notificar" />
         </div>
     )
   }
