3fb4240ceb3010d5259bbad4e9cc77b28fa44b6b
Stenio Ellison <stenio1998@gmail.com>
2022-09-10 23:45:48 -0300

    Toast2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
       <>
-        <ToastContainer/>
+        <ToastContainer  />
         <Header/>
         <main>
           <section id="sales">
